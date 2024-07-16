990754d569313ae49b8c524e10c89aef0ad4dc75
puneetVyasArchitech <pvyas@architech.ca>
2024-07-16 16:41:32 +0530

    Added Asynchronous call

--- src/main/java/com/bulk/Controller.java
+++ src/main/java/com/bulk/Controller.java
@@ -1,5 +1,10 @@
 package com.bulk;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,20 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-	private DemoService demoService;
+    private DemoService demoService;
+    private ExecutorService executorService;
 
-	public Controller(DemoService demoService) {
-		this.demoService = demoService;
-	}
+    public Controller(DemoService demoService) {
+        this.demoService = demoService;
+        this.executorService = Executors.newFixedThreadPool(10); 
+    }
 
-	@GetMapping("/demo")
-	public ResponseEntity<String[]> demo() {
+    @GetMapping("/demo")
+    public ResponseEntity<String[]> demo() {
 
-		String response10 = demoService.fetchDataIn10Sec();
-		String response5 = demoService.fetchDataIn5Sec();
-		
-		String[] responses = { response10, response5 };
+        Future<String> response10Future = executorService.submit(() -> demoService.fetchDataIn10Sec());
+        Future<String> response5Future = executorService.submit(() -> demoService.fetchDataIn5Sec());
 
-        return ResponseEntity.ok(responses);
-	}
+        try {
+            String response10 = response10Future.get();
+            String response5 = response5Future.get();
+            String[] responses = { response10, response5 };
+            return ResponseEntity.ok(responses);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(new String[] { "Error occurred: " + e.getMessage() });
+        }
+    }
 }
